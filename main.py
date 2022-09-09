a43a235cfb7c76f6e4c5f6b0d78154421f752895
Amera <amera.boudia84@gmail.com>
2022-09-09 11:21:08 +0200

    Projet flask

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request,send_file,redirect,url_for,Response,redirect
+from chooseMonstre import *
+
+app = Flask(__name__)
+
+import sys
+import os
+
+@app.route('/',methods=['GET','POST'])
+
+def Menu():
+    if request.form :
+       pseudo = request.form.get('name')
+       liste_monsrtres = []
+       nbre_monstre = 4
+
+       for i in range(nbre_monstre):
+           nom_monstre = chooseMonstre(liste_nom)
+           liste_monsrtres.append(nom_monstre)
+
+       liste_pseudo = [pseudo,nbre_monstre]
+       return render_template('index.html',pseudo=pseudo,nbre_monstre=nbre_monstre,liste_pseudo=liste_pseudo,liste_monsrtres=liste_monsrtres)
+    else:
+       return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8001)
