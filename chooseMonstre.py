7f4fac172018b433383438c16df285ed2de9f24d
Amera <amera.boudia84@gmail.com>
2022-09-09 11:40:53 +0200

    commit1

--- chooseMonstre.py
+++ chooseMonstre.py
@@ -1,6 +1,6 @@
 import random
-liste_nom = ['monstre1','monstre2','monstre3','monstre4','monstre4']
 
+liste_nom = ['monstre1','monstre2','monstre3','monstre4','monstre4']
 def chooseMonstre(liste_nom):
     nom_monstre = random.choice(liste_nom)
     return nom_monstre
--- main.py
+++ main.py
@@ -6,20 +6,22 @@ app = Flask(__name__)
 import sys
 import os
 
+
 @app.route('/',methods=['GET','POST'])
 
 def Menu():
     if request.form :
        pseudo = request.form.get('name')
-       liste_monsrtres = []
+       liste_monstres = []
        nbre_monstre = 4
 
        for i in range(nbre_monstre):
            nom_monstre = chooseMonstre(liste_nom)
-           liste_monsrtres.append(nom_monstre)
+           liste_monstres.append(nom_monstre)
+       
 
        liste_pseudo = [pseudo,nbre_monstre]
-       return render_template('index.html',pseudo=pseudo,nbre_monstre=nbre_monstre,liste_pseudo=liste_pseudo,liste_monsrtres=liste_monsrtres)
+       return render_template('index.html',pseudo=pseudo,nbre_monstre=nbre_monstre,liste_pseudo=liste_pseudo,liste_monstres=liste_monstres)
     else:
        return render_template('index.html')
 if __name__ == "__main__":
